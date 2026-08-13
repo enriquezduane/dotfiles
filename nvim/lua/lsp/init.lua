6ecd107a778fd6802ec480db57aebf3ee750de6c
Duane Enriquez <duanequez@gmail.com>
2026-08-13 18:36:00 +0800

    add python lsp and add autoformat capability

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,8 +1,12 @@
+-- lua/lsp/init.lua
+
 -- Enable the LSPs
 vim.lsp.enable({
     "clangd",
     "lua_ls",
-    "gopls"
+    "gopls",
+    "ruff",
+    "basedpyright",
 })
 
 -- Diagnostic configuration
@@ -23,4 +27,20 @@ vim.diagnostic.config({
     },
     update_in_insert = false,
     severity_sort = true
+})
+
+-- Format on save automatically whenever an LSP supports it
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspFormat", { clear = true }),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client:supports_method("textDocument/formatting") then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                end,
+            })
+        end
+    end,
 })
