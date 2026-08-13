8288011f5e01125c5ab5844f0f9d558a223f6a56
Duane Enriquez <duanequez@gmail.com>
2026-08-13 18:22:04 +0800

    uncomment k

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,8 @@
 -- Visual mode
 vim.keymap.set("v", "p", '"_dP', { noremap = true, silent = true })
 
--- Movement vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+-- Movement
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Buffer navigation
