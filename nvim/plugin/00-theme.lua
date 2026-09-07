c862fa83124c0f14521612a2c618a56bcb3b231a
Duane Enriquez <duanequez@gmail.com>
2026-09-07 13:50:10 +0800

    use custom default when on macos

--- nvim/plugin/00-theme.lua
+++ nvim/plugin/00-theme.lua
@@ -1,4 +1,12 @@
 vim.pack.add({ "https://github.com/neanias/everforest-nvim" })
 
 require("everforest").setup({})
-vim.cmd.colorscheme("everforest")
+
+local is_mac = vim.uv.os_uname().sysname == "Darwin"
+
+if is_mac then
+    vim.cmd.colorscheme("custom-default")
+else
+    require("everforest").setup({})
+    vim.cmd.colorscheme("everforest")
+end
