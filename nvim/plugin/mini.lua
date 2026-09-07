2fd2623db88362bbca43d2641b14acf62e321149
Duane Enriquez <duanequez@gmail.com>
2026-09-07 13:50:31 +0800

    show tabline only if buffer count > 1

--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -11,5 +11,15 @@ require("mini.extra").setup()
 
 require("mini.tabline").setup()
 
+local function update_tabline()
+    local n = #vim.fn.getbufinfo({ buflisted = 1 })
+    vim.o.showtabline = n > 1 and 2 or 0
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter", "BufAdd", "BufDelete" }, {
+    callback = function()
+        vim.schedule(update_tabline)
+    end,
+})
 -- Smart buffer removal (preserves window layout)
 require("mini.bufremove").setup()
